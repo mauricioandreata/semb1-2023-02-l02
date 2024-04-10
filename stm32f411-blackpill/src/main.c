f1f5e211f8235bb34931424b772e4feb3ed4f87a
Mauricio Andreata <mauriciocnandreata@gmail.com>
2024-04-10 11:03:59 -0300

    Update main.c

--- stm32f411-blackpill/src/main.c
+++ stm32f411-blackpill/src/main.c
@@ -23,22 +23,28 @@
 
 /* GPIOC Base Addresses ******************************************************/
 
-#define STM32_GPIOC_BASE     0x40020800     /* 0x48000800-0x48000bff: GPIO Port C */
+#define STM32_GPIOA_BASE     0x40020000     /* 0x40020000 - 0x400203FF: GPIO Port A */
+#define STM32_GPIOC_BASE     0x40020800     /* 0x40020800 - 0x40020BFF: GPIO Port C */
 
 /* Register Offsets *********************************************************/
 
-#define STM32_RCC_AHB1ENR_OFFSET  0x0030   /* AHB1 Peripheral Clock enable register */
+#define STM32_RCC_AHB1ENR_OFFSET  0x0030   /* AHB1 Peripheral Clock enable
+                                               register */
 
 #define STM32_GPIO_MODER_OFFSET   0x0000  /* GPIO port mode register */
 #define STM32_GPIO_OTYPER_OFFSET  0x0004  /* GPIO port output type register */
 #define STM32_GPIO_PUPDR_OFFSET   0x000c  /* GPIO port pull-up/pull-down register */
+#define STM32_GPIO_IDR_OFFSET     0x0010  /* GPIO port input data register */
 #define STM32_GPIO_ODR_OFFSET     0x0014  /* GPIO port output data register */
 #define STM32_GPIO_BSRR_OFFSET    0x0018  /* GPIO port bit set/reset register */
-
 
 /* Register Addresses *******************************************************/
 
 #define STM32_RCC_AHB1ENR        (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
+
+#define STM32_GPIOA_MODER        (STM32_GPIOA_BASE+STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOA_PUPDR        (STM32_GPIOA_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOA_IDR          (STM32_GPIOA_BASE+STM32_GPIO_IDR_OFFSET)
 
 #define STM32_GPIOC_MODER        (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
 #define STM32_GPIOC_OTYPER       (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
@@ -48,6 +54,7 @@
 
 /* AHB1 Peripheral Clock enable register */
 
+#define RCC_AHB1ENR_GPIOAEN      (1 << 0)  /* Bit 0:  IO port A clock enable */
 #define RCC_AHB1ENR_GPIOCEN      (1 << 2)  /* Bit 2:  IO port C clock enable */
 
 /* GPIO port mode register */
@@ -84,7 +91,7 @@
 
 /* Configuration ************************************************************/
 
-#define LED_DELAY  100000
+#define LED_DELAY  500000
 
 /****************************************************************************
  * Private Types
@@ -108,21 +115,25 @@
 
 int main(int argc, char *argv[])
 {
-  uint32_t i;
   uint32_t reg;
 
   /* Ponteiros para registradores */
 
   uint32_t *pRCC_AHB1ENR  = (uint32_t *)STM32_RCC_AHB1ENR;
+
+  uint32_t *pGPIOA_MODER  = (uint32_t *)STM32_GPIOA_MODER;
+  uint32_t *pGPIOA_PUPDR  = (uint32_t *)STM32_GPIOA_PUPDR;
+  uint32_t *pGPIOA_IDR  = (uint32_t *)STM32_GPIOA_IDR;
+
   uint32_t *pGPIOC_MODER  = (uint32_t *)STM32_GPIOC_MODER;
   uint32_t *pGPIOC_OTYPER = (uint32_t *)STM32_GPIOC_OTYPER;
   uint32_t *pGPIOC_PUPDR  = (uint32_t *)STM32_GPIOC_PUPDR;
   uint32_t *pGPIOC_BSRR   = (uint32_t *)STM32_GPIOC_BSRR;
 
-  /* Habilita clock GPIOC */
+  /* Habilita clock GPIOA e GPIOC */
 
   reg  = *pRCC_AHB1ENR;
-  reg |= RCC_AHB1ENR_GPIOCEN;
+  reg |= (RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN);
   *pRCC_AHB1ENR = reg;
 
   /* Configura PC13 como saida pull-up off e pull-down off */
@@ -142,17 +153,26 @@ int main(int argc, char *argv[])
   reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(13));
   *pGPIOC_PUPDR = reg;
 
+   /* Configura PA0 como entrada pull-up on e pull-down off */
+
+  reg = *pGPIOA_MODER;
+  reg &= ~GPIO_MODER_MASK(0);
+  reg |= (GPIO_MODER_INPUT << GPIO_MODER_SHIFT(0));
+  *pGPIOA_MODER = reg;  
+
+  reg = *pGPIOA_PUPDR;
+  reg &= ~GPIO_PUPDR_MASK(0);
+  reg |= (GPIO_PUPDR_PULLUP << GPIO_PUPDR_SHIFT(0));
+  *pGPIOA_PUPDR = reg;
+
   while(1)
     {
-      /* Liga LED */
+      if(!(*pGPIOA_IDR &(1<<1))){
+        *pGPIOC_BSRR = GPIO_BSRR_RESET(13); /* Liga LED */
+      }else{
+        *pGPIOC_BSRR = GPIO_BSRR_SET(13); /* Desliga LED */
+      }
 
-      *pGPIOC_BSRR = GPIO_BSRR_RESET(13);
-      for (i = 0; i < LED_DELAY; i++);
-
-      /* Desliga LED */
-
-      *pGPIOC_BSRR = GPIO_BSRR_SET(13);
-      for (i = 0; i < LED_DELAY; i++);
     }
 
   /* Nunca deveria chegar aqui */
